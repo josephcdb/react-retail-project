b515f287930034aa3d10b93562684b00c2873380
Joseph <chua.debao@gmail.com>
2026-04-29 11:38:01 +0200

    Import queryClient from queryClient file as a clean code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 import "./index.css";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
