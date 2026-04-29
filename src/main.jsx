12b9e5bb57e7a3c9018c22df91fd616c73445553
Joseph <chua.debao@gmail.com>
2026-04-29 15:37:00 +0200

    Fix the typo naming

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { CartProvider } from "./context/CartProvider";
+import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
